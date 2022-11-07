55744f70c64248c23c7cbf6928a455fee2d4cbda
Admin <nicholasfran45@gmail.com>
2022-11-07 17:03:54 -0500

    Made a basic GUI layout

--- game.py
+++ game.py
@@ -1,2 +1,12 @@
 import PySimpleGUI as sg
 
+layout = [[sg.Text('This is a thing')]]
+
+window = sg.Window('Thingy', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
